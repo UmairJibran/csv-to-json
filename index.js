c9ad6fbbe89a47ee8f937101b9d2abfa38ead5d4
Umair Jibran <umairjibran7@gmail.com>
2020-07-23 22:29:48 +0500

    completed CSV to JSON

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//CSV to JSON Converter
+//CSV to JSON Converter via npm package csvtojson
+//requiring core package
+const path = require("path");
+//requiring npm package
+const csv = require("csvtojson/v2");
+//requiring custom modules
+const writeToFile = require("./write_to_file.js");
+//runner
+async function runner() {
+   const csvFilePath = "./customer-data.csv"; //defining path till the file
+   let jsonObj = await csv().fromFile(csvFilePath);
+   writeToFile(
+      JSON.stringify(jsonObj),
+      path.join(__dirname, "customer-data.json")
+   );
+}
+
+runner();
